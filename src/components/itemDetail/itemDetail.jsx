302021de310ad91794469a935a001f5852796657
yanina paz ugarte <yanina.ugarte24@gmail.com>
2023-11-21 10:35:52 -0300

    mejoras en css

--- src/components/itemDetail/itemDetail.jsx
+++ src/components/itemDetail/itemDetail.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom"
 import Counter from '../counter/counter'
 import style from "./styles.module.css"
+import { Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 
 const ItemDetail = ({producto}) => {
@@ -14,6 +16,9 @@ const ItemDetail = ({producto}) => {
                 <p>Descripcion: {producto?.description}</p>
                 <img className={style.img} src={producto?.image} />
                 <Counter product={producto}/>
+                <Link to={"/cart"}>
+                    <Button>Agregar al carrito</Button>
+                </Link>
             </>
         ) : (
             <p>El producto con id:{id}no existe</p>
